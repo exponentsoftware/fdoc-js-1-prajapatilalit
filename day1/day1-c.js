38b9de4e8af58aff0ec4dc3b04f4209607286278
prajapatilalit <prajapatilalit150992@gmail.com>
2021-09-18 16:46:54 +0530

    day1 part 1d coungtWords solution

--- day1/day1-c.js
+++ day1/day1-c.js
@@ -1,6 +1,6 @@
 // count word length in the given sentence.
 
-const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`
+const sentence =  `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`;
 
 function countWords(para){
     
